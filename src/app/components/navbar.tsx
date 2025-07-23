e661ba94a1ccb77c9f6cf1e84b4603ac5f0a7f75
Jeevan Baabu <jeevanbaabu03@gmail.com>
2025-07-23 23:02:51 +0530

    added the changing navbar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,45 +1,51 @@
 'use client';
 
 import { motion } from 'framer-motion';
-import { useEffect, useState,useRef } from 'react';
-import Hero from "./Hero_Page"
-export default function navbar() {
-    const [open, setOpen] = useState(false)
+import { useEffect, useState } from 'react';
+
+type NavbarProps = {
+  active: string; 
+};
+
+
+
+export default function Navbar({ active }: NavbarProps) {
+  const [open, setOpen] = useState(false);
   const [navScrolled, setNavScrolled] = useState(false);
 
-   
+ useEffect(() => {
+  if (active === "Home") {
+    setNavScrolled(true);
+  } else {
+    setNavScrolled(false);
+  }
+}, [active]);
+  
 
-    return (
-        <div className='flex sticky top-0 z-[9999] justify-center py-9 '>
+  return (
+    <div className='flex sticky top-0 z-[9999] justify-center py-9'>
 
-              <motion.div className='bg-white rounded-full  absolute py-5 px-5'>
-                <ul className='flex items-center space-x-5 font-inter tracking-wider text-black'>
-                    <li className="flex items-center ">
-                        <span className='w-5 h-5 bg-black rounded-full '></span>
-                    </li>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Tech-Stack</a></li>
-                    <li><a href="#">Project</a></li>
-                    <li><a href="#">Contact</a></li>
-                </ul>
-                 </motion.div>
-           
+    { navScrolled ? (
+      <motion.div className='bg-white rounded-full absolute py-5 px-5'>
+        <ul className='flex items-center space-x-5 font-inter tracking-wider text-black'>
+          <li className="flex items-center ">
+            <span className='w-5 h-5 bg-black rounded-full'></span>
+          </li>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Tech-Stack</a></li>
+          <li><a href="#">Project</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </motion.div>
+    ) : (
 
-                <motion.div>
+        <motion.div className='bg-white rounded-full absolute py-5 px-5'>
+            <a className='text-black'>{active}</a>
 
-                </motion.div>
-           
-               
-                    
-               
-                
-
-
-
-
-           
-            
-        </div>
-    );
+        </motion.div>
+    )
+      }
+    </div>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,98 @@
-'use client'; 
+'use client';
 import Navbar from "./components/navbar"
 import Hero from "./components/Hero_Page"
 import About from "./components/about"
-import  Tech from "./components/tech-stack"
-import  Project from "./components/project"
+import Tech from "./components/tech-stack"
+import Project from "./components/project"
 import Contact from "./components/contact"
 
-import { useState , useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 
 
 export default function Home() {
- const heroref = useRef(null);
-  
-  return (
+  const HeroRef = useRef(null);
+  const AboutRef = useRef(null);
+  const TechRef = useRef(null);
+  const ProjectRef = useRef(null);
+  const ContactRef = useRef(null);
+
+  const [active,setActive] = useState("")
+
+  useEffect ( () => {
+
+    const section = [
+      {id:"Home",ref:HeroRef}, 
+      {id:"About",ref:AboutRef},
+      {id:"Tech",ref:TechRef},
+      {id:"Project",ref:ProjectRef},
+      {id:"Contact",ref:ContactRef},
+    ];
 
     
-   
-   <main>
-      <Navbar></Navbar>
-      <section className="min-h-screen">
+     
+    const obs = new IntersectionObserver (
+
+      (dom) => { dom.forEach((view) =>{
+
+        if (view.isIntersecting)
+        {
+          const page= section.find(sec => sec.ref.current === view.target)
+
+          
+          if(page)
+          {
+            setActive(page.id)
+          }
+        }
+
+
+
+      });
+
+
+
+
+
+
+      },
+      { threshold: 0.6}
+    );
+    section.forEach(({ref}) => {
+      if (ref.current) obs.observe(ref.current);
+    
+    })
+
+    return () => {
+      section.forEach(({ref}) => {
+        if(ref.current) obs.observe(ref.current);
+      });
+    };
+
+
+
+
+  }, []);
+
+
+  return (
+
+
+
+    <main>
+      <Navbar active={active} />
+      <section ref={HeroRef} className="min-h-screen">
         <Hero></Hero>
       </section>
-      <section className="min-h-screen">
+      <section ref={AboutRef} className="min-h-screen">
         <About></About>
       </section>
-      <section className="min-h-screen">
+      <section ref={TechRef} className="min-h-screen">
         <Tech></Tech>
       </section>
-      <section className="min-h-screen">
+      <section ref={ProjectRef} className="min-h-screen">
         <Project></Project>
       </section>
-      <section className="min-h-screen">
+      <section ref={ContactRef} className="min-h-screen">
         <Contact></Contact>
       </section>
     </main>
